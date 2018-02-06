6170acc2c905ec6e17cbe099bb080b6cb3fe37c3
Guilherme Castro <guilherme@datalinux.com.br>
2018-02-06 07:42:54 -0300

    - Continuação do desenvolvimento do módulo de matrículas

--- src/Javascript/Functions.Matricula.js
+++ src/Javascript/Functions.Matricula.js
@@ -43,3 +43,77 @@ function LimpaCurso(IdElementForm) {
 	$("#"+IdElementForm+" input[name=curs_duracao]").val('');
 }
 /*### FUNÇÃO QUE LIMPA OS DADOS DO CURSO NO FORMULÁRIO DE MATRÍCULA ###*/
+
+
+
+/*### FUNÇÃO QUE CALCULA O TROCO NO PAGAMENTO ###*/
+function CalculaTroco(valorRecebido) {
+
+	// console.log("keyup");
+	var valor = valorRecebido;
+
+	valor = valor.replace(".","");
+	valor = valor.replace(",",".");
+
+	var troco = Number(valor) - Number($("#res_total_pagar").val());
+
+	var trocoFormat = troco;
+
+	// console.log("troco");
+	// console.log(troco);
+
+	$("#res_autoriza_pgto").val(0);
+
+	if(Number(valor)>0) {
+
+		if(Number(troco)<0) {
+
+			$("#res_troco").html("<p class='bg-danger'>Valor insuficiente para realizar o pagamento</p>");
+		}
+		else if(Number(troco)==0) {
+
+			$("#res_troco").html("R$ 0 (troco não necessário)");
+			$("#res_autoriza_pgto").val(1);
+		}
+		else {
+
+			// console.log("troco cedulas");
+
+			var cedulasDisponiveis = [100,50,10,5,1,0.50,0.10,0.05,0.01];
+
+			var cedulasTroco = new Array();
+
+			var texto_cedulas = "";
+
+			for (x in cedulasDisponiveis) {
+
+				if (cedulasDisponiveis[x] > troco) continue;
+
+				var quantidadeCedula = parseInt(troco / cedulasDisponiveis[x]);
+				cedulasTroco.push([quantidadeCedula, cedulasDisponiveis[x]]);
+				troco = troco - (quantidadeCedula * cedulasDisponiveis[x]);
+
+				if(cedulasDisponiveis[x]>1) {
+
+					texto_cedulas += quantidadeCedula + " nota(s) de " + (cedulasDisponiveis[x]).formatMoney(2, ',', '.') + ", ";
+				}
+				else {
+
+					texto_cedulas += quantidadeCedula + " moeda(s) de " + (cedulasDisponiveis[x]).formatMoney(2, ',', '.') + ", ";
+				}
+			}
+
+			// console.log(cedulasTroco);
+			// console.log(texto_cedulas.slice(0,-2));
+
+			$("#res_troco").html("R$ " + (trocoFormat).formatMoney(2, ',', '.') + "<br>" + texto_cedulas.slice(0,-2));
+			$("#res_autoriza_pgto").val(1);
+		}
+	}
+	else {
+
+		$("#res_troco").html("Digite o valor recebido");
+	}
+}
+/*### FUNÇÃO QUE CALCULA O TROCO NO PAGAMENTO ###*/
+
